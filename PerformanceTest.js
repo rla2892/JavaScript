832f54a9c553bfc80c252a64bf84a359823ee5fc
Kim Sangjin <37770080+rla2892@users.noreply.github.com>
2019-03-20 17:05:30 +0900

    Update PerformanceTest.js

--- PerformanceTest.js
+++ PerformanceTest.js
@@ -1,9 +1,9 @@
 //퍼포먼스 테스트하는 함수
 function performanceTest(f) {
-  var t0 = performance.now();
-  f();
-  var t1 = performance.now();
-  console.log("Call to Function took " + (t1 - t0) + " milliseconds.");
+  var t0 = performance.now()
+  f()
+  var t1 = performance.now()
+  console.log('Call to Function took ' + (t1 - t0) + ' milliseconds.')
 }
 
 //테스트용 
