885b6fbab5d70d7e12dcb42c054c248c31824a14
Kim Sangjin <37770080+rla2892@users.noreply.github.com>
2019-03-22 18:12:56 +0900

    Update PrintArray.js

--- PrintArray.js
+++ PrintArray.js
@@ -1,9 +1,21 @@
 //예제 배열
 const arr1 = [1,2,3,4,5]
+const cars = ["Benz", "Volvo", "BMW", "Toyota", "Hyundai"]
+
+//배열 기본 프로퍼티
+var x = cars.length;   // 길이
+var y = cars.sort();   // 정렬
 
 //foreach 
-const printArray = function(arr) {
+const printArray1 = function(arr) {
   arr.forEach(element => {    
     console.log(element)
   })
+}
+
+//for
+const printArray2 = function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
 }
