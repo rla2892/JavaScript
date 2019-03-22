a418cde9902c56174d8daeb187ed5fc79361bb80
Kim Sangjin <37770080+rla2892@users.noreply.github.com>
2019-03-22 18:14:45 +0900

    Update PrintArray.js

--- PrintArray.js
+++ PrintArray.js
@@ -3,8 +3,8 @@ const arr1 = [1,2,3,4,5]
 const cars = ["Benz", "Volvo", "BMW", "Toyota", "Hyundai"]
 
 //배열 기본 프로퍼티
-var x = cars.length;   // 길이
-var y = cars.sort();   // 정렬
+var x = cars.length // 길이
+var y = cars.sort() // 정렬
 
 //foreach 
 const printArray1 = function(arr) {
@@ -16,6 +16,7 @@ const printArray1 = function(arr) {
 //for
 const printArray2 = function(arr) {
   for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i])
+    const element = arr[i]
+    console.log(element)
   }
 }
