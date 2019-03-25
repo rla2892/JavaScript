092b668988340cdf1ac3ae65fb1cc86a0cfd6fc5
Kim Sangjin <37770080+rla2892@users.noreply.github.com>
2019-03-25 13:40:36 +0900

    Update EventTest.js

--- EventTest.js
+++ EventTest.js
@@ -8,3 +8,11 @@ document.addEventListener('wheel', testFunc)
 //간단
 document.addEventListener('wheel', o => alert())
 document.addEventListener('click', o => alert())
+
+//지울 수 있는 이벤트 리스너
+//테스트 내용
+const testFunc = function() {
+  console.log('test start ------------------------')
+}
+document.addEventListener('auxclick', testFunc)
+document.addEventListener('wheel', o => document.removeEventListener('auxclick', testFunc))
